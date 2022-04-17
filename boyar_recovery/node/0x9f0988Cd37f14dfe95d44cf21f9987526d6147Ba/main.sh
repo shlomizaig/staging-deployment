3a4a6f6d438fb9ebe2450f887dfed74fd556084a
uvorbs <yuval@orbs.com>
2022-04-17 15:19:06 +0300

    bash cleanup

--- boyar_recovery/node/0x9f0988Cd37f14dfe95d44cf21f9987526d6147Ba/main.sh
+++ boyar_recovery/node/0x9f0988Cd37f14dfe95d44cf21f9987526d6147Ba/main.sh
@@ -4,11 +4,36 @@
  
 # Define bash shell variable called var 
 # Avoid spaces around the assignment operator (=)
-var="staging script22"
+var="staging script55"
  
 # Another way of printing it
 printf "%s\n" "$var"
 
 # create a file
 mkdir -p target_files
-touch target_files/script_file33.txt
+touch target_files/script_file55.txt
+
+############################################
+## cleanup
+
+# limit journam 200m
+journalctl --vacuum-size=200M
+
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
+
+# apt-get cleanup - sudo ommited as boyar is already sudo
+apt-get clean
+# clean apt cache 
+apt-get autoclean
+# unnecessary packages
+apt-get autoremove
+# old kernel versions
+dpkg --get-selections | grep linux-image
+# snapd
+apt purge snapd
